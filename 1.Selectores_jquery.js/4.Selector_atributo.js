1c44bd45a62d2b78bb4a8c2b10ed160e240136e5
jhonnyer <jhonnyerg@unicauca.edu.co>
2021-06-17 00:48:59 -0500

    Delete 4.Selector_atributo.js

--- 1.Selectores_jquery.js/4.Selector_atributo.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-$(document).ready(function(){
-    // Selector de atributo: Selecciona las etiquetas o elementos de HTML que tengan asignados ciertos atributos
-    // selecionar los elementos por atributos, utilizando los corchetes 
-    // Atributo title 
-    $('[title="Google"]').css('background','#ccc');
-    $('[title="Facebook"]').css('background','blue');
-});
